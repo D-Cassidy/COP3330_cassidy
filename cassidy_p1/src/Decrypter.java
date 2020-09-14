d02f802c58c9338b598e3f2ab046d7d40ef8bc53
Whimsickal <declanpcassidy1@gmail.com>
2020-09-14 05:18:23 -0400

    Removes some notes in Decrypter

--- cassidy_p1/src/Decrypter.java
+++ cassidy_p1/src/Decrypter.java
@@ -1,7 +1,5 @@
 public class Decrypter {
-    // 1. Decrypt the string by inverting the encrypt function.
-    // 2. Return the decrypted integer as a String.
-
+     
     public static String decryptDigits(String digits) {
          int num;
          String newString = "";
