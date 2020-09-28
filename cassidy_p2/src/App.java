8e9209abda7ca54ae0381b1f3ebff4b57b3e0119
Whimsickal <declanpcassidy1@gmail.com>
2020-09-28 16:53:51 -0400

    Implements functioning code for App and BodyMassIndex classes

--- cassidy_p2/src/App.java
+++ cassidy_p2/src/App.java
@@ -1,4 +1,59 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
+
+    // Takes Y or N input and returns corresponding Boolean
+    public static boolean moreInput() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Would you like to input more data? Y or N");
+        char input = scan.next().charAt(0);
+
+        return !(input == 'N' || input == 'n');
+    }
+
+    // Takes input for the user's height
+    public static double getUserHeight() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter a valid height");
+        double h = scan.nextDouble();
+        scan.nextLine();
+
+        return (h > 0) ? h : getUserHeight();
+    }
+
+    // Takes input for the user's weight
+    public static double getUserWeight() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter a valid weight");
+        double w = scan.nextDouble();
+        scan.nextLine();
+
+        return (w > 0) ? w : getUserWeight();
+    }
+
+    // Displays BMI and BMI Category from BodyMassIndex class
+    public static void displayBmiInfo(BodyMassIndex bmi) {
+        System.out.println("BMI: " + bmi.bmi);
+        System.out.println("BMI Category: " + bmi.bmiCategory);
+    }
+
+    // Displays average BMI of all BMIs entered
+    public static void displayBmiStatistics(ArrayList<BodyMassIndex> bmiData) {
+        double average = 0;
+
+        int len = bmiData.size();
+        for (int i = 0; i < len; i++)
+            average += bmiData.get(i).bmi;
+
+        average = average / len;
+        System.out.println("Average BMI: " + average);
+    }
+
+    // NO TOUCHING
     public static void main(String[] args) {
         ArrayList<BodyMassIndex> bmiData = new ArrayList<BodyMassIndex>();
 
--- cassidy_p2/src/BodyMassIndex.java
+++ cassidy_p2/src/BodyMassIndex.java
@@ -1,2 +1,26 @@
+import java.lang.Math;
+
 public class BodyMassIndex {
+    double bmi;
+    String bmiCategory;
+
+    public double calculateBmi(double h, double w) {
+        return (703 * w) / Math.pow(h, 2);
+    }
+
+    public String calculateBmiCategory(double bmiScore) {
+        if (bmiScore < 18.5)
+            return "Underweight";
+        else if (bmiScore >= 18.5 && bmiScore < 25)
+            return "Normal weight";
+        else if (bmiScore >= 25 && bmiScore < 30)
+            return "Overweight";
+        else
+            return "Obesity";
+    }
+
+    public BodyMassIndex (double h, double w) {
+        bmi = calculateBmi(h, w);
+        bmiCategory = calculateBmiCategory(bmi);
+    }
 }
