6fea5aacb7ffc034fa55c50e5477661a9c0257a3
Whimsickal <declanpcassidy1@gmail.com>
2020-11-16 09:49:37 -0500

    Initial commit

---
+++ cassidy_p4/src/TaskItem.java
@@ -0,0 +1,5 @@
+public class TaskItem {
+    String title; // Shall be 1 or more characters
+    String desc; // Shall be 0 or more characters
+    String dueDate; // Must be in the format 'YYYY-MM-DD'
+}
